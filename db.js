e6cedc161977eb89539cb5c4a83f39956a87f816
Md Zahid Hussain <134547438+CallmeZahid@users.noreply.github.com>
2024-12-23 01:45:42 +0530

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectToDB = async()=>{
     try {
-        await mongoose.connect(`${process.env.MONGO_URI}`);
+        await mongoose.connect(`mongodb+srv://hussain321zahid:MaHT8QYlGWAim23J@cluster0.mkfhz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
           console.log('✅ Connected to Database');
     } catch (error) {
         console.error('❌ Failed to connect to database:', error.message);
