88d1b260e2ab9376994249e71efceb4790e03b35
Md Zahid Hussain <134547438+CallmeZahid@users.noreply.github.com>
2024-12-23 03:40:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,15 @@ connectToDB();
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.use(cors());
+
+//app.use(cors());
+//new code
+app.use(cors({
+    origin: ['https://zippy-crepe-528398.netlify.app/'], // Add your Netlify URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+  }));
+
 app.use(helmet());
 
 const server = new ApolloServer({
@@ -39,46 +47,13 @@ app.get('/',(req,res)=>{
 server.start().then(()=>{
     server.applyMiddleware({ app, path: '/api' });
     app.listen(port,()=>{
-        console.log(`server is listening at http://cart-lister.onrender.com/${port}${server.graphqlPath}`);
+        console.log(`server is listening at http://cart-lister.onrender.com/${server.graphqlPath}`);
     })
 })
 
-// new line
-
-
-// Middleware
-// app.use(bodyParser.json());
-
 // // MongoDB Connection
-const uri = 'process.env.MONGO_URL'; // Replace with your URI
+//const uri = 'process.env.MONGO_URL'; // Replace with your URI
+const uri = 'mongodb+srv://hussain321zahid:MaHT8QYlGWAim23J@cluster0.mkfhz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'; // Replace with your URI
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
-
-// // Example Schema and Model
-// const ItemSchema = new mongoose.Schema({ name: String });
-// //const Item = mongoose.model('Item', ItemSchema);
-
-// // Example Routes
-// app.get('/items', async (req, res) => {
-//   try {
-//     const items = await Item.find();
-//     res.json(items);
-//   } catch (err) {
-//     res.status(500).send(err.message);
-//   }
-// });
-
-// app.post('/items', async (req, res) => {
-//   try {
-//     const newItem = new Item(req.body);
-//     await newItem.save();
-//     res.json(newItem);
-//   } catch (err) {
-//     res.status(500).send(err.message);
-//   }
-// });
-
-// // Start Server
-// // const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
